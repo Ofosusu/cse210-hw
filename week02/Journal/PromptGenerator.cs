858661d8ca6a603fc4065f1d2ccb79608a009a82
ofosusu <amoahdavid00@gmail.com>
2026-09-14 23:25:36 +0000

    Update

--- week02/Journal/PromptGenerator.cs
+++ week02/Journal/PromptGenerator.cs
@@ -7,7 +7,7 @@ public class PromptGenerator
 
     public PromptGenerator()
     {
-        _prompts.Add("Who was the most interesting person I interacted with today?");
+        _prompts.Add("Who was the most interesting person I interacted with?");
         _prompts.Add("What was the best part of my day?");
         _prompts.Add("How did I see the hand of the Lord in my life today?");
         _prompts.Add("What was the strongest emotion I felt today?");
